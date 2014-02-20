593bbbdbaeb039162ba6309545d18632d33425c8
Casing Chu <casing@gmail.com>
2014-02-19 23:37:12 -0500

    Second Solution to find Missing value in Arithmetic Sequence which finds the missing value in O(n/2) in worst case instead of O(n)

--- src/main/java/com/casing/sampleproblems/ArithmeticSequenceMissing.java
+++ src/main/java/com/casing/sampleproblems/ArithmeticSequenceMissing.java
@@ -34,6 +34,31 @@ public class ArithmeticSequenceMissing {
         return 0;
     }
     
+    public static int findASMissing2(int[] ar) {
+        if(ar.length >= 3 && ar.length <= 2500) {
+            int i = 2;
+            int s1 = 0;
+            int s2 = 0;
+            int len = ar.length % 2 == 0 ? ar.length - 1 : ar.length;
+            while(i < len) {
+                s1 = ar[i-1] - ar[i-2];
+                s2 = ar[i] - ar[i-1];
+                
+                if(s1 > s2) {
+                    return ar[i-2] + s2;
+                } else if(s1 < s2) {
+                    return ar[i-1] + s1;
+                }
+                
+                i+=2;
+            }
+            
+            // If we are here then there is one last index to check
+            return ar[ar.length-2] + s1;
+        }
+        return 0;
+    }
+    
     public static void main(String [] args) {
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
@@ -41,6 +66,6 @@ public class ArithmeticSequenceMissing {
         for(int i=0;i<size;i++) {
             ar[i] = sc.nextInt();
         }
-        System.out.println(findASMissing(ar));
+        System.out.println(findASMissing2(ar));
     }
 }
