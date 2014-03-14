a814435dea17b8cfedfc3ea6f7eb7406dbd94b3d
Casing Chu <casing@gmail.com>
2014-03-13 23:50:08 -0400

    Update description of problem

--- src/main/java/com/casing/sampleproblems/MasterMind.java
+++ src/main/java/com/casing/sampleproblems/MasterMind.java
@@ -9,7 +9,11 @@ package com.casing.sampleproblems;
 import java.util.Scanner;
 
 /**
- * Mastermind is a game of two players. In the beginning, first player decides a secret key, which is a sequence (s1,s2,...sk) where 0 < si <= n, Then second player makes guesses in rounds, where each guess is of form (g1,g2, ...gk), and after each guess first player calculates the score for the guess. Score for a guess is equal to number of i's for which we have gi = si.
+ * 
+    Mastermind is a game of two players. In the beginning, first player decides a secret key, which is a sequence (s1,s2,...sk) where 0 < si <= n, 
+    Then second player makes guesses in rounds, where each guess is of form (g1,g2, ...gk), and after each guess first player calculates the score for the guess. 
+    Score for a guess is equal to number of i's for which we have gi = si.
+    
     For example if the secret key is (4,2,5,3,1) and the guess is (1,2,3,7,1),then the score is 2, because
     g2 = s2 and g5 = s5. 
 
@@ -17,9 +21,11 @@ import java.util.Scanner;
 
     Input
 
-    First line of input contains a single integer C (1 <=C <= 100). C test-cases follow. First line of each test-case contains three integers n,k and q. (1 <=n,k <=11, 1<=q<=8). Next q lines contain the guesses.
+    First line of input contains a single integer C (1 <=C <= 100). C test-cases follow. 
+    First line of each test-case contains three integers n,k and q. (1 <=n,k <=11, 1<=q<=8). Next q lines contain the guesses.
 
-    Each guess consists of k integers gi,1, gi,2,....gi,k separated by a single space, followed by the score for the guess bi (1 <= gi,j <=n for all 1 <=i <=q, 1 <=j <=k; and 0 <= bi <=k )
+    Each guess consists of k integers gi,1, gi,2,....gi,k separated by a single space, 
+    followed by the score for the guess bi (1 <= gi,j <=n for all 1 <=i <=q, 1 <=j <=k; and 0 <= bi <=k )
 
     Output
 
